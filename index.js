63d55044349dcb6314c209b29a616d3c93dabefd
SaniyaFarooqui <saniyafarooqui1107@gmail.com>
2023-06-24 20:56:19 +0530

    index.js file added

---
+++ index.js
@@ -0,0 +1,60 @@
+// to print any statement we console
+console.log("saniya is a good girl");
+
+//single line comment we use ("//")
+//to define variable
+
+//multiline comment 
+/*arbaz
+saniya
+sofiya*/
+
+//to define variable we use anyone of the following
+//let   to define a variable whose value can be change
+//const  to define a variable whose value can't be change
+//var   to define variable
+
+//let saniya = 10
+//saniya = 20
+//console.log("saniya");
+
+const saniya1 = 10
+console.log("this is from const :" , saniya1);
+
+var arbaz = saniya1
+console.log("this is from var :" , arbaz);
+
+//variable defination 
+let saniya
+
+//variable declaration
+saniya = "arbaz khan"
+console.log("this is from let :" , saniya);
+
+//template literal
+console.log(`this is saniya she loves sleeping ${arbaz}`);
+//to get information abt any variable  or string or object we use
+console.info("saniya")
+
+//operator in javascript
+//addition operation
+let saniya12 = 12 + 12
+console.log("this addition is :" ,saniya12);
+
+//subraction operation
+let saniya13 = 12 - 12
+console.log("this subraction is :" ,saniya13);
+
+
+//multiplication operation
+let saniya111 = 12 * 12
+console.log("this multiplication is :" ,saniya111);
+
+//divide operation
+let saniya_divide = 12/12
+console.log("this division is :" ,saniya_divide);
+
+//remainder operation
+let remainder = 12 % 12
+console.log("this remainder is :" ,remainder);
+
