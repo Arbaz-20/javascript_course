100752f7c77c4c6896ed876f6efd93e244eea608
SaniyaFarooqui <saniyafarooqui1107@gmail.com>
2023-07-05 21:23:25 +0530

    today test fail

--- for_practice.js
+++ for_practice.js
@@ -55,3 +55,73 @@ addition = (p1,p2,p3) => {
 }
 let sofiya =addition(10,87,64)
 console.log(sofiya);
+
+for(let k = 0; k<10; k++){
+    console.log(`the number is ${k}`);
+}
+
+let l = 0;
+while(l < 10){
+    console.log(`this is while loop number ${l}`);
+    l++
+}
+
+let d = 0
+do{
+    console.log(`this is do while loop number ${d}`);
+    d++
+}while(d<10);
+
+let sofiyaarray = ["saniya","alam","farhan","sana","alfiya","sofiya"];
+for(let response of sofiyaarray){
+    console.log(response);
+}
+
+let yasinarray = ["yasmin","yasin","nazreen"];
+for(let response in yasinarray){
+    console.log(response);
+}
+
+let object ={"firstname" : "saniya" ,"lastname" : "farooqui"}
+ for(let response in object){
+    console.log(`${response}` +':'+ `${object[response]}`);
+ }
+let m = 13;
+if (m == 12){
+    console.log("saniya");
+}else if(m == 13){
+    console.log("sofiya");
+}else {
+    console.log("not found");
+}
+
+let o = 17;
+if (o >= 18){
+    console.log(`then able to vote`);
+}else {
+    console.log(`then not able to vote`);
+}
+
+let q = 10;
+if(q <= 15){
+    console.log("can enjoy waterpark");
+}else{
+    console.log("sorry you are not allowed");
+}
+
+let u = 15;
+switch (u) {
+    case 1 :
+        //block of code
+        break;
+    case 2 :
+        //block of code
+        break;
+    case 15 :
+        console.log("Yes");
+        break;
+    default :
+        console.log("nothing was there");
+}
+
+    
