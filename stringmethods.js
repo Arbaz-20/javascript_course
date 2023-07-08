b120ea3eb1e97e5f966ab11e5023cb368917547b
SaniyaFarooqui <saniyafarooqui1107@gmail.com>
2023-07-08 21:06:20 +0530

    string methods

---
+++ stringmethods.js
@@ -0,0 +1,8 @@
+let name = "  Saniya_Farooqui_Anisuddin   "
+console.log(`the length of the name is :`+ name.length);
+
+let trimdataEnd = name.trimEnd()
+console.log("the trim length of variable is : ",trimdata.length);
+
+let trimdataStart = name.trimStart()
+console.log("the start trim data is : ",trimdataStart.length);
