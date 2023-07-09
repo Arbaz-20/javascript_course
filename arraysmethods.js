ef9d17a96a77378aaab848884f2adb916026d3a6
SaniyaFarooqui <saniyafarooqui1107@gmail.com>
2023-07-09 17:25:37 +0530

    string method completed and array method half

---
+++ arraysmethods.js
@@ -0,0 +1,37 @@
+let array = ["arbaz","saniya","alfiya","sofiya","nazreen","nasreen","raza","ayush","asif"];
+let size = array.length;
+console.log(size);
+
+let string = array.toString();
+console.log(string);
+
+let pop =array.pop();
+console.log("This is the pop : ",array);
+
+let push = array.push("anis")
+console.log(array);
+
+let appendvalue = array[array.length] = "alam"
+
+let shift = array.shift()
+console.log(array);
+
+let unshift = array.unshift("arbaz khan")
+console.log(array);
+
+let deletevalue = delete array[2]
+console.log(array);
+
+let appendalfiya_again = array[2] = 'alfiya'
+console.log(array);
+
+let array2 = ["arbaz","saniya","alfiya"]
+let arra3 = ["anis","gudiya phuppu","famida","danish"]
+
+let concatinate = array.concat(array2,arra3);
+console.log(concatinate);
+
+let flat = [[1,2],[3,4],[5,6]]
+
+let flatarray = flat.flat()
+console.log(flatarray);
--- stringmethods.js
+++ stringmethods.js
@@ -1,8 +1,80 @@
 let name = "  Saniya_Farooqui_Anisuddin   "
 console.log(`the length of the name is :`+ name.length);
 
-let trimdataEnd = name.trimEnd()
-console.log("the trim length of variable is : ",trimdata.length);
+let text = "lets play       "
+let text1 = text.trim();
+console.log("this is trim :",text1);
 
 let trimdataStart = name.trimStart()
 console.log("the start trim data is : ",trimdataStart.length);
+
+let text2 = "apple , banana , kiwi";
+let part = text2.slice(7);
+console.log("the slice is :",part);
+
+let part1 = text2.substring(7,14);
+console.log("the substring is :",part1);
+
+let parts = text2.substr(7,7);
+console.log("the substr is :",parts);
+
+let text3 = "i am in school";
+let data = text3.replace("school","college");
+console.log("the new text is :",data);
+
+let replacetext = "arbaz is going to work very hard and arbaz is strong"
+let replcedata = replacetext.replaceAll("arbaz","saniya")
+console.log("--------------------------------"+replcedata+"-----------------------------------------");
+
+let stringvalue = "arbaz and saniya are learning frontend"
+let splitvalue = stringvalue.includes("saniya")
+if (splitvalue == true){
+    console.log("this is true:", splitvalue);
+}else{
+    console.log("this is false:",splitvalue);
+}
+
+switch(splitvalue){
+    case true:
+        console.log("this is true ",splitvalue);
+        break;
+    case false:
+        console.log("this is false" , splitvalue);
+        break;
+    default:
+        console.log("not found");
+
+}
+
+let string1= "saniya"
+let string2 = "farooqui"
+let strings = string1.concat(" ",string2)
+console.log(strings);
+
+
+let array = ["saniya", "arbaz", "sofiya", "alfiya", "raisa"];
+for(let response of array){
+    console.log(array.indexOf(response)+`:`+ response);
+}
+
+
+let string = "Saniya farooqui"
+let char = string.charAt(0);
+console.log(char);
+
+let chars = string.charCodeAt(0)
+console.log(chars);
+
+
+let stringsplit = "arbaz saniya alfiya sufiya alam"
+let value = stringsplit.split(" ")
+console.log(value);
+
+let commastring = "arbaz,saniya,lol"
+let commavalue = commastring.split(",")
+console.log(commavalue);
+
+let pipingstring = "arbaz | string | sufiya"
+let pipingvalue = pipingstring.split("|");
+console.log(pipingvalue);
+
