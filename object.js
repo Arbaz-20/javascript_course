b749204638b45db0dfac0e3a82c2f63ac19352c9
SaniyaFarooqui <saniyafarooqui1107@gmail.com>
2023-07-19 21:41:57 +0530

    object method started

--- object.js
+++ object.js
@@ -1,29 +1,50 @@
-let object ={"name" : "saniya","middle name" :"anisuddin", "surname" : "farooqui","age" :18};
- for(response in object){
-    console.log(response +":"+ object[response]);
- }
+let finalArray = []
+let nameArray = []
 
-let arr = ["name","husband","children"]
-let array = ["raisa","anis","Sofiya"]
-let arrayobj =[]
-let obj ={}
+let key = ["name","surname","age","qualification","gender","email"]
+let value =[["Arbaz","Khan",21,"Graduation","Male","arbaz@gmail.com"],["Arbaz","Khan",21,"Graduation","Male","arbaz@gmail.com"],["Arbaz","Khan",21,"Graduation","Male","arbaz@gmail.com"],["Arbaz","Khan",21,"Graduation","Male","arbaz@gmail.com"],["Arbaz","Khan",21,"Graduation","Male","arbaz@gmail.com"],["Arbaz","Khan",21,"Graduation","Male","arbaz@gmail.com"]]
 
-for(i = 0 ; i< 3 ; i++){
-    obj[`${arr[i]}`] = array[i]
-    arrayobj.push(obj) 
+for(let response of value){
+    //console.log(response);
+    let object = {}
+    for(let i = 0; i < value.length; i++){
+        object[`${key[i]}`] = response[i]
+        delete object.age
+    }
+    finalArray.push(object)
+}
+console.log(finalArray);
+
+
+let saniya = {
+    name : "saniya",
+    surname : "farooqui",
+    age:17
+}
+delete saniya.age
+console.log(saniya);
+
+//nested object
+
+let arbaz = {
+    name:"Arbaz",
+    surname:"Khan",
+    age:21,
+    saniya:{
+        name:"Saniya",
+        surname:"Farooqui",
+        age:17
+    },
+    alfiya:{
+        name:"Alfiya",
+        surname:"Farooqui",
+        age:21,
+    }
 }
 
-console.log(arrayobj);
+arbaz.saniya["name"] = "sofiya"
+arbaz.saniya["age"] = 19
+console.log(arbaz);
+console.log(arbaz.saniya);
+console.log(arbaz.alfiya);
 
-let value ={
-    "hobbies" : "writnig",
-    "college" : "sophia"
-}
-value.age = 18
-value.name ="saniya"
-value.surname = "farooqui"
-console.log(value);
-
-for(response in value){
-    console.log(response + ":" + value[response]);
-}
