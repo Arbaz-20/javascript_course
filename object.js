8e7c440e28c94a10327de3672f5e935ec1bc3bd9
SaniyaFarooqui <saniyafarooqui1107@gmail.com>
2023-07-22 21:07:24 +0530

    object method completed

--- object.js
+++ object.js
@@ -97,4 +97,81 @@ for (let i in object.myobj) {
   array.push(x)
 }
 
-console.log(array);
+//console.log(array);
+
+let arrkey = ["name","surname","age"]
+let arrvalue = [["saniya","farooqui",17],["saniya","farooqui",17],["saniya","farooqui",17]]
+let arr =[]
+
+for(response of arrvalue){
+    let object ={}
+    for(i=0 ; i<response.length ; i++){
+        object[arrkey[i]]= response[i]
+    }
+    arr.push(object)
+}
+console.log(arr);
+
+
+
+let data =["alfiya","farooqui",21]
+if(data != "raisa"){
+    console.log("true");
+}else{
+    console.log("false");
+}
+ let arrdata = data.indexOf("anisuddin")
+ console.log(arrdata);
+
+let arraydata = data[3]="anisuddin"
+//console.log(data);
+
+let obj = {
+    name:"Arbaz",
+    surname:"Khan"
+}
+let saniyaobj = {
+    name:"Saniya",
+    surname:"Farooqui",
+    ages1:32
+}
+let sufiyaobj = {
+    name:"Sufiya",
+    surname:"Farooqui",
+    age:20
+}
+
+let concat = Object.assign(obj,{saniyaobj,sufiyaobj})
+console.log(concat);
+
+function arbazkhan12 (name,age,surname){
+    this.name = name,
+    this.age = age,
+    this.surname = surname
+}
+
+let arbazvalue = new arbazkhan12("arbaz",21,"khan")
+console.log(arbazvalue);
+
+
+let keys_of_object = Object.keys(concat)
+console.log(keys_of_object);
+
+let values_of_object = Object.values(concat)
+console.log(values_of_object);
+
+let enteries = Object.entries(concat);
+console.log(enteries);
+
+let get = Object.getOwnPropertyDescriptors(concat,"name");
+console.log(get);
+
+let defineobject = {name:"Arbaz",surname:"Khan"}
+Object.defineProperty(defineobject,"name",{writable:false});
+defineobject["name"] = "sufiya"
+console.log(Object.getOwnPropertyDescriptors(defineobject));
+console.log(defineobject);
+
+Object.preventExtensions(defineobject);
+defineobject.age = 22
+console.log(defineobject);
