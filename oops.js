977a9f975a56713d136560d7c5221114d956a467
Arbaz Khan <96368513+Arbaz-20@users.noreply.github.com>
2023-07-23 17:05:18 +0530

    commit from arbaz

--- oops.js
+++ oops.js
@@ -1,2 +1,22 @@
+class Saniya{
 
+    constructor() {
+
+    }
+
+    SaniyaMethod =(value) => {
+        if(value == ""){
+            console.log("Value is empty")
+        }
+        else{
+            console.log("This is the value",value)
+        }
+    }
+
+    ArbazMethod = () => {
+
+    }
+
+}
+export default Saniya
 
