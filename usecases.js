7f1a65464d1027fb7220db83b5f5bc5f348995a3
SaniyaFarooqui <saniyafarooqui1107@gmail.com>
2023-07-25 20:38:37 +0530

    basic complete

--- usecases.js
+++ usecases.js
@@ -81,3 +81,59 @@ console.log(saniyamap);
 let hash = new Map(Object.entries(saniya))
 console.log(hash);
 console.log(hash.size);
+
+let keys = ["name", "surname","age"]
+let values = [["saniya","farooqui", 17],
+              ["saniya","farooqui", 17],
+              ["saniya","farooqui", 17],
+              ["saniya","farooqui", 17]
+            ]
+
+arraeofobject = (keyname, valuename) => {
+    if( keyname == "" && valuename == ""){
+        return "value required in array"
+    }else{
+        let arrayofarray =[]
+        for (let response of valuename ){
+            let object = {}
+            for(let i = 0 ; i < response.length; i++){
+            object[`${keyname[i]}`] = response[i]
+            }
+            arrayofarray.push(object)
+        }
+        return arrayofarray
+    }
+}
+let answer = arraeofobject(keys , values)
+console.log(answer);
+
+
+let object = {
+    name : "saniya",
+    surname : "farooqui",
+    middlename : "anisuddin",
+    age : 17,
+}
+let anotherobject = {
+    phone_number : 9152116196,
+    stream :"bscit",
+    hobbies : "writing",
+    email : "saniyafarooqui05@gmail.com"
+}
+
+MergeObject = (firstobject , secondobject ) => {
+    if(firstobject == null || secondobject == null ){
+        return "provided object is empty"
+    }else{
+        try {
+            let finalobject = {}
+            finalobject = Object.assign(firstobject,secondobject)
+            return finalobject    
+        } catch (error) {
+            return Error.name;
+        }
+    }
+
+}
+let call = MergeObject(object , anotherobject)
+console.log(call);
